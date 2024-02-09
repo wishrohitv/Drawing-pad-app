43a1fa42167fee7cc76b70cc8c769f2f290962a3
Rohit Vishwakarma <126248971+wishrohitv@users.noreply.github.com>
2024-02-09 09:16:03 +0530

    Delete components directory

--- components/widgets/classes_tabs.py
+++
@@ -1,9 +0,0 @@
-from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.video import Video
-
-
-class ClassesTabs(TabbedPanel):
-    pass
-    # def on_enter(self):
-    #     self.video = Video(source="https://youtu.be/4HRC6c5-2lQ?si=oEMtulA9vTc0G0EE")
-        # self.ids.videos.text = self.video
--- components/widgets/nav_bar.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
-
-
-class NavBar(BoxLayout):
-    greeting = StringProperty()
-
