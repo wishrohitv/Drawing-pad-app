80d8cd38f19866bf87be4586ad0939bec1a7a3da
Rohit Vishwakarma <126248971+wishrohitv@users.noreply.github.com>
2024-02-09 09:16:45 +0530

    Delete extra directory

--- extra/any.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-from PIL import Image
-
-urllib.request.urlretrieve(
-    'https://media.geeksforgeeks.org/wp-content/uploads/20210318103632/gfg-300x300.png',
-    "gfg.png")
-
-img = Image.open("gfg.png")
-img.show()
