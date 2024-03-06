b0c4b0391dfdada0fd7b81d68baf79c3bcbb7dd4
Rohit Vishwakarma <126248971+wishrohitv@users.noreply.github.com>
2024-03-06 11:22:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from kivy.uix.screenmanager import Screen
+from turtle import pos, width
 from kivy.app import App
+from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 from kivy.graphics import Rectangle, Color, Line
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.colorpicker import ColorPicker
 from kivy.uix.popup import Popup
 from kivy.uix.slider import Slider
+from sqlalchemy import values
 
 
 class CanvasScreen(BoxLayout):
@@ -16,6 +19,10 @@ class CanvasScreen(BoxLayout):
         self.prev_touch_pos = None
         self.selected_colors = 0,0,0,1
         self.pencil_width_ = 2
+        # line box
+        with self.canvas:
+            Color(rgba=(1,1,0,1))
+            Line(width=1.2, rounded_rectangle=(self.x, self.y, self.width, self.height, 6))
 
 
     def on_touch_down(self, touch):
@@ -32,7 +39,7 @@ class CanvasScreen(BoxLayout):
             with self.canvas:
                 # Color(rgba = self.selected_colors)
                 # Line(width=2, points=(self.prev_touch_pos[0], self.prev_touch_pos[1], touch.x, touch.y))
-                Line(width=2, points=(self.prev_touch_pos[0], self.prev_touch_pos[1], touch.x, touch.y))
+                Line(width=self.pencil_width_, points=(self.prev_touch_pos[0], self.prev_touch_pos[1], touch.x, touch.y))
         self.prev_touch_pos = touch.pos
 
     def on_touch_up(self, touch):
@@ -45,38 +52,47 @@ class ScreenWindow(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
 
+        self.canvas_screen = CanvasScreen()   
         # color picker
-        self.color_picker = ColorPicker()
+        self.color_picker = ColorPicker(color = (0,0,0,1))
         self.color_picker.bind(color=self.on_color)
 
         # color_pallete
         self.color_pallate = Popup(title="pick color", title_align="center", content=self.color_picker, size_hint=(.7,.7))
 
         # sider
-        self.slider = Slider(value_track=True, step=.1, min=.1, max=10)
+        self.slider = Slider(value_track=True, step=.1, min=.1, max=10, value=2)
         self.slider.bind(value=self.pencil_width)
-
-        # canvas screen
-        self.canvas_screen = CanvasScreen()   
-        self.add_widget(self.canvas_screen)
+        self.slider_label = Label(color="black", size_hint=(.2, 1), font_size=18, text="2")
         
-        float = BoxLayout(padding=5, spacing=10)
-        float.add_widget(Button(text="clear", size_hint=(1,None), height=40, pos_hint={'top':1}, on_press=self.clear))
-        self.add_widget(float)
+        # control buttons
 
-        # color _picker Button
-        self.color_picker_button = Button(text="pick\ncolor", size_hint=(1,None), height=40, pos_hint={'top':1},background_normal="", background_color="black", on_press=self.open_color_pallete)
-
-        float.add_widget(self.color_picker_button)
-
+        color_ = BoxLayout(padding=5, spacing=10, size_hint=(1,None), height=self.height)
         colors = ["red", "green", "yellow", "black", "pink", "blue"]
 
         for color in colors:
-            float.add_widget(ColoredBorderButton(text=f"{color}", pos_hint={'top':1}, group="colors", on_press=self.choosed_colors, background_normal="", background_color=color))
+            color_.add_widget(ColoredBorderButton(text=f"{color}", pos_hint={'top':1}, group="colors", on_press=self.choosed_colors, background_normal="", background_color=color))
 
-        slid_box = BoxLayout(size_hint=(1,None), height="40dp")
+        # 2nd row
+        slid_box = BoxLayout(size_hint=(1,None), height=40, padding=10)
+    
+        slid_box.add_widget(Button(text="clear", size_hint=(None,1), width=70, on_press=self.clear))
+        # color _picker Button
+        self.color_picker_button = Button(text="pick\ncolor", size_hint=(None,1), width=70, font_size=14,background_normal="", background_color="black", on_press=self.open_color_pallete)
+        slid_box.add_widget(self.color_picker_button)
         slid_box.add_widget(self.slider)
-        self.add_widget(slid_box)
+        slid_box.add_widget(self.slider_label)
+
+        # canvas screen
+
+        # root box
+        self.root_box = BoxLayout(orientation="vertical", padding=10)
+        self.root_box.add_widget(color_)
+        self.root_box.add_widget(slid_box)
+        self.root_box.add_widget(self.canvas_screen)
+
+        self.add_widget(self.root_box)
+
 
     def choosed_colors(self, instance):
         match instance.text:
@@ -106,15 +122,14 @@ class ScreenWindow(Screen):
         self.color_pallate.dismiss()
 
     def pencil_width(self, instance, value):
-        print(value)
         self.canvas_screen.pencil_width_ = value
+        self.slider_label.text = str(round(value, 1))
 
 class ColoredBorderButton(ToggleButton):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.size_hint=(1,None)
         self.height=40
-        self.pos_hint= {'top':1}
 
         # with self.canvas.before:
         #     Color(0,0,0,1)
